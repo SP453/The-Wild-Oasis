8b02e427ac286e3ac470bbde9eab436cff6def87
Preetam <preetamsarode453@gmail.com>
2024-02-09 22:17:21 +0530

    update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://assvyzmjqmpplotcuadf.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzc3Z5em1qcW1wcGxvdGN1YWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcwMzg1MTEsImV4cCI6MjAyMjYxNDUxMX0.0alyNSxRehUnrujLRv46ybQENoP8-Pj1l3mrRqo0Bu8";
+export const supabaseUrl = "SUPABSE_URL";
+const supabaseKey = "SUPABASE_KEY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
